395da6cbc68fce2d5a0877edef231602c20a0334
XiaoXuanAndMolu <756842118@qq.com>
2018-06-28 18:25:02 +0800

    02

--- src/com/jiyun/Test.java
+++ src/com/jiyun/Test.java
@@ -4,6 +4,8 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println("2585--01");
+        System.out.println("7568--01");
+        System.out.println("7568--02");
     }
 
 }
